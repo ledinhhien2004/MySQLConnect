a1cdc92cd6c5b221e6191e3d90f7264f1d187acf
ledinhhien2004 <tuoidinh1976@gmail.com>
2024-10-17 23:28:38 +0700

    Connect

---
+++ src/hien/dev/MainApp.java
@@ -0,0 +1,21 @@
+package hien.dev;
+
+
+import java.sql.DriverManager;
+
+public class MainApp {
+	
+    public static void main(String[] args){
+    	final String DB_URL = "jdbc:mysql://localhost:3306/shop";
+        final String USER = "root";
+        final String PASS = "";
+        try{ 
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            DriverManager.getConnection(DB_URL, USER, PASS);
+            System.out.println("Ket noi thanh cong!!");
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+}
+        
